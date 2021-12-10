a0d463f738b22e1d6cd9af9c99e01819f5460f0a
Sifat <ibnsifat@gmail.com>
2021-12-10 21:10:21 +0600

    creating layouts for product and cart

--- frontend/src/Components/HomePage/Shared/Products/Products.js
+++ frontend/src/Components/HomePage/Shared/Products/Products.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 // stylesheet
 import './product.css'
 const Products = () => {
     return (
         <>
+            {/* search div  */}
             <div className="searchDiv h-25 py-4 ">
                 <h2 className='fs-2 fw-bold text-light mt-3'>Search</h2>
                 <input className='search' placeholder='Search by materials' type="text" name="search" id="search" />
             </div>
+            {/* dividing display into product and cart */}
+            <Container fluid>
+                {/* creating row column */}
+                <Row>
+                    {/* product column  */}
+                    <Col xs={7}>
+                        <Row>
+
+                        </Row>
+                    </Col>
+                    {/* cart column */}
+                    <Col xs={5}>
+
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 };
