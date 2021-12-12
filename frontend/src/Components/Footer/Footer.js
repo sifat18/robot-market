864aeb0409a1045de308e6707d75ea305b79f1f7
Sifat <ibnsifat@gmail.com>
2021-12-12 13:20:39 +0600

    updating contact at footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -16,6 +16,17 @@ const Footer = () => {
                 <Col xs={12} md={4} className='social ps-5'>
                     <img src={footImg} alt="" height='100' width='100' />
                 </Col>
+                <Col xs={12} md={6}>
+                    <Row >
+                        {/* footer notes */}
+                        <Col xs={12} md={6} className='mb-5'>
+                            <p className='fw-bold fs-4'>About us</p>
+                            <p className='fw-bold fs-4'>Our Services</p>
+                            <p className='fw-bold fs-4'>Cell:+880-129423954</p>
+                        </Col>
+
+                    </Row>
+                </Col>
 
 
             </Row>
